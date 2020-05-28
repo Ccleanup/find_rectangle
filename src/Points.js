83a2e067dd35e154b280fd4662dcf1d287cc1d22
sweechuan <chiasweechuan07@gmil.com>
2020-05-28 13:43:44 +0800

    changed combination function changed find rectangle function

--- src/Points.js
+++ src/Points.js
@@ -8,65 +8,51 @@ const points = {
     p5: { id: '5', x: '3', y: '0' },
     p6: { id: '6', x: '-3', y: '-3' },
     p7: { id: '7', x: '0', y: '-3' },
-    p8: { id: '8', x: '3', y: '-3' }
+    p8: { id: '8', x: '3', y: '-3' },
+    // p9: { id: '9', x: '-7', y: '-1' },   // test for additional points
+    // p10: { id: '10', x: '-4', y: '-4' }
 }
 
 const Points = () => {
-    const all_points = Object.keys(points).map((p, i) => {
+    const listAllPoints = Object.keys(points).map((p, i) => {
         return (
             <div>this is point - {p} with id: {points[p].id}, x: {points[p].x}, y: {points[p].y}</div>
         )
     })
 
     const arrIds = Object.keys(points).map((p, i) => points[p].id)
+
+    // generate all possible combinations of arrIds (no repeat)
+    let arrIds1 = arrIds
+    let arrIds2 = arrIds
+    let arrIds3 = arrIds
+    let arrIds4 = arrIds
     let arr4combs =[]
-    let arr4comb1 = []
-    let arr4comb2 = []
-    let arr4comb3 = []
-    for (let i=0; i < arrIds.length-3; i++) {                           // eg [1,2,3,4,5,6,7,8]
-        //arr4comb1 = arrIds.slice(i, i+1)
-        for (let j=0; j < arrIds.length-i-3; j++) {                     // when i=0, j=1
-            arr4comb1 = arrIds.slice(i, i+1)                            // [1]
-            arr4comb1 = arr4comb1.concat(arrIds.slice(i+j+1, i+j+4))    // [1] + [3,4,5]
-
-            arr4comb2 = arrIds.slice(i, i+2)                            // [1,2]
-            arr4comb2 = arr4comb2.concat(arrIds.slice(i+j+2, i+j+4))    // [1,2] + [4,5]
-
-            arr4comb3 = arrIds.slice(i, i+3)                            // [1,2,3]
-            arr4comb3 = arr4comb3.concat(arrIds.slice(i+j+3, i+j+4))    // [1,2,3] + [5]
-
-            if (j === 0) {
-                arr4combs.push(arr4comb1)
-            } else {
-                arr4combs.push(arr4comb1)                               // [1,3,4,5]
-                arr4combs.push(arr4comb2)                               // [1,2,4,5]
-                arr4combs.push(arr4comb3)                               // [1,2,3,5]
+    let arr4comb =[]
+    
+    for (let i=0; i < arrIds1.length; i++) {
+        for (let j=i+1; j < arrIds2.length; j++) {  // i+1 so that no repeat
+            for (let k=j+1; k < arrIds3.length; k++) {
+                for (let l=k+1; l < arrIds4.length; l++) {
+                    arr4comb =[]
+                    arr4comb.push(arrIds1[i])
+                    arr4comb.push(arrIds2[j])
+                    arr4comb.push(arrIds3[k])
+                    arr4comb.push(arrIds4[l])
+                    arr4combs.push(arr4comb)
+                }
             }
         }
     }
 
-    const sortArrCombs = arr4combs.map((comb, i) => {
+    const listArrCombs = arr4combs.map((comb, i) => {
         return (
         <div>{i+1}) {comb.toString()}</div>
         )
     })
 
-    // const findRect = comb, index => {
-    //     comb
-
-    //     const getRect = arr4combs.map((comb, i) => {
-    //         return (
-    //             x1 = points[`p${comb[0]}`].x
-    //             x2 = points[`p${comb[1]}`].x
-
-    //             x = x1 - x2
-    //         )
-    //     })
-    // }
-
-    const arrRect = arr4combs.filter((comb, i) => {
-        let isRect = false
-
+    const findRect = arr4combs.filter((comb, i) => {
+        
         let x1 = points[`p${comb[0]}`].x
         let x2 = points[`p${comb[1]}`].x
         let x3 = points[`p${comb[2]}`].x
@@ -77,23 +63,29 @@ const Points = () => {
         let y3 = points[`p${comb[2]}`].y
         let y4 = points[`p${comb[3]}`].y
 
-        let xa = Math.abs(x1 - x2)
-        let xb = Math.abs(x3 - x4)
-        let ya = Math.abs(y1 - y3)
-        let yb = Math.abs(y2 - y4)
+        let xa = Math.abs(x1 - x2)  // 6x distance in a rectangles: 4 sides + 2 cross
+        let xb = Math.abs(x1 - x3)
+        let xc = Math.abs(x3 - x4)
+        let xd = Math.abs(x2 - x4)
+        let xe = Math.abs(x1 - x4)
+        let xf = Math.abs(x2 - x3)
 
-        if ( xa === xb && ya === yb) {
-            console.log('it is rectangle')
+        let ya = Math.abs(y1 - y2)
+        let yb = Math.abs(y1 - y3)
+        let yc = Math.abs(y3 - y4)
+        let yd = Math.abs(y2 - y4)
+        let ye = Math.abs(y1 - y4)
+        let yf = Math.abs(y2 - y3)
+
+        let isRect = false
+        if ( (xa === yc && xb === yd && xc === ya && xd === yb) ||
+        (xa === xc && xb === xd && xe === xf && ya === yc && yb === yd && ye === yf ) )
             isRect = true
-        } else {
-            console.log('it is not rectangle')
-            //arr4combs.filter(comb => comb)
-        }
 
         return isRect === true
     });
 
-    const sortArrRect = arrRect.map((comb, i) => {
+    const listFindRect = findRect.map((comb, i) => {
         return (
         <div>{i+1}) {comb.toString()}</div>
         )
@@ -103,15 +95,15 @@ const Points = () => {
 
     return (
         <>
-        <div>{all_points}</div>
+        <div>{listAllPoints}</div>
         <br></br>
         <div>4 points combinations:</div>
-        <div>{sortArrCombs}</div>
+        <div>{listArrCombs}</div>
         <br></br>
         <div>Rectangles:</div>
-        <div>{sortArrRect}</div>
+        <div>{listFindRect}</div>
         <br></br>
-        <div>Total Rectangles: {arrRect.length}</div>
+        <div>Total Rectangles: {findRect.length}</div>
         </>
     )
 }
